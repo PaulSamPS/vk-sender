c65957f17cac07619f10843ab01ce11857464aee
Pavel Samoylenko <paulsamv@yandex.ru>
2024-07-30 15:33:10 +0300

    удалил все ненужное

--- src/vk-message/vk-message.controller.ts
+++ src/vk-message/vk-message.controller.ts
@@ -1,14 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { VkMessageDto } from './vk-message.dto';
-import { extractVkResourceId } from './extract-vk-resource-id';
 import { VkMessageService } from './vk-message.service';
-
-const urls = [
-  'https://vk.com/photos?z=photo471261080_457242826%2Fphoto_feed471261080',
-  'https://vk.com/photos?z=photo471261080_457242825%2Fphoto_feed471261080',
-  'https://vk.com/photos?z=photo471261080_457240045%2Fphoto_feed471261080',
-  'https://vk.com/video?z=video-86538677_456241043%2Fpl_cat_trends',
-];
 
 @Controller('vk-message')
 export class VkMessageController {
@@ -22,11 +14,5 @@ export class VkMessageController {
       attachment: dto.attachment,
       user_ids,
     });
-  }
-
-  @Get()
-  getUrls() {
-    const stringUrls = extractVkResourceId(urls);
-    console.log(stringUrls);
   }
 }
